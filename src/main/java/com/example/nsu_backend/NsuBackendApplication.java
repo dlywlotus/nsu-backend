f64e7a33ea77e77f77d6c2540906b6a18a963b36
Dylan <dylan.low@univers.com>
2026-04-21 11:33:02 +0800

    Set up application-local.yml, .env variables and docker compose

--- src/main/java/com/example/nsu_backend/NsuBackendApplication.java
+++ src/main/java/com/example/nsu_backend/NsuBackendApplication.java
@@ -10,9 +10,6 @@ public class NsuBackendApplication {
         SpringApplication.run(NsuBackendApplication.class, args);
     }
 
-    // TODO: Remove One to Many mappings
-    // TODO: Set up application-local.yml, .env variables and docker compose
-
     // TODO: remove the need for device id for authentication.
     // TODO: Add unit and integration tests for auth
 
