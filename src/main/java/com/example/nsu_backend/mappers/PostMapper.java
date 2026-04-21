707919c7de421fa8893f3b7399c05162c196ea38
Dylan <dylan.low@univers.com>
2026-04-21 12:06:31 +0800

    Minor refactoring

--- src/main/java/com/example/nsu_backend/mappers/PostMapper.java
+++ src/main/java/com/example/nsu_backend/mappers/PostMapper.java
@@ -1,13 +1,12 @@
 package com.example.nsu_backend.mappers;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingConstants;
-
 import com.example.nsu_backend.dto.AddPostRequest;
 import com.example.nsu_backend.dto.PostDetails;
 import com.example.nsu_backend.entities.Post;
 import com.example.nsu_backend.entities.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface PostMapper {
@@ -17,9 +16,6 @@ public interface PostMapper {
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "author", source = "author")
-    @Mapping(target = "comments", ignore = true)
     @Mapping(target = "likeCount", constant = "0")
     Post addPostDtoToNewPost(AddPostRequest dto, User author);
-
-
 }
