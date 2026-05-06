99ca9350460021971c42a08083db05daaef4c6f9
Dylan <dylan.low@univers.com>
2026-05-06 17:53:57 +0800

    Fix jakarta validation

--- src/main/java/com/example/nsu_backend/dto/AddCommentRequest.java
+++ src/main/java/com/example/nsu_backend/dto/AddCommentRequest.java
@@ -1,14 +1,14 @@
 package com.example.nsu_backend.dto;
 
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.util.UUID;
 
 public record AddCommentRequest(
         @NotBlank
-        @Max(value = 1000, message = "Comments have a max character length of 1000 characters!")
+        @Size(max = 1000, message = "Comments have a max character length of 1000 characters!")
         String body,
 
         @NotNull
