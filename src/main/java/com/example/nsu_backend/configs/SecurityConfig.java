5e932cd55a1277209144b8727310ca79ac2a7ee3
dlywlotus <dylanlowywgit@gmail.com>
2026-05-21 11:28:33 +0800

    Configure cors

--- src/main/java/com/example/nsu_backend/configs/SecurityConfig.java
+++ src/main/java/com/example/nsu_backend/configs/SecurityConfig.java
@@ -1,8 +1,8 @@
 package com.example.nsu_backend.configs;
 
-import com.example.nsu_backend.security.CustomAuthenticationEntryPoint;
-import com.example.nsu_backend.security.JwtFilter;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +12,14 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.AuthorizationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.example.nsu_backend.security.CustomAuthenticationEntryPoint;
+import com.example.nsu_backend.security.JwtFilter;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
@@ -19,10 +27,13 @@ import org.springframework.security.web.access.intercept.AuthorizationFilter;
 public class SecurityConfig {
     private final JwtFilter jwtFilter;
     private final CustomAuthenticationEntryPoint entryPoint;
+    @Value("${frontend.server.url}")
+    private String frontEndServerUrl;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
         return http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(requests ->
                         requests.requestMatchers("/sign_in", "/sign_up", "/error", "/refresh_token", "/api-docs/**", "/swagger-ui/**").permitAll()
@@ -30,6 +41,18 @@ public class SecurityConfig {
                 .addFilterBefore(jwtFilter, AuthorizationFilter.class)
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(entryPoint))
                 .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of(frontEndServerUrl));
+        config.setAllowedMethods(List.of("*"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 
     @Bean
